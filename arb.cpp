076f1aed7e07fa9bb44d544a3b1803a5ead68e67
Peter Anselmo <peter_anselmo@yahoo.com>
2018-01-24 16:08:49 -0800

    Pulled out all the over-pointering I was doing. Basic logic works

--- arb.cpp
+++ arb.cpp
@@ -15,7 +15,7 @@ class crypto_exchange {
         _name = new_name;
 
         if(_name == "coinbase"){
-            _market_fee = 0.25;
+            _market_fee = 0.0025;
         } else {
             cout << "Unknown Exchange" << endl;
         }
@@ -39,21 +39,18 @@ struct trade_pair {
 };
 
 struct trade_seq {
-    vector<trade_pair*> pairs;
+    vector<trade_pair> pairs;
     float net_gain = 1;
 
-    void add(const trade_pair& new_trade_pair){
-        cout << "Saving Pair to Seq..." << endl;
-        pairs.push_back(&new_trade_pair);
-        cout << "Exchange" << new_trade_pair.exchange->name();
-        cout << "fee" << new_trade_pair.exchange->market_fee();
-        net_gain = net_gain * new_trade_pair.ask * new_trade_pair.exchange->market_fee();
+    void add_pair(trade_pair new_trade_pair){
+        pairs.push_back(new_trade_pair);
+        net_gain = net_gain * new_trade_pair.ask * (1 - new_trade_pair.exchange->market_fee());
     }
 
     void print_seq(){
         cout << "Trade Seq: ";
-        for(const trade_pair* tp : pairs){
-            cout << tp->sell << ">" << tp->buy << "@" << tp->ask << ", ";
+        for(const trade_pair& tp : pairs){
+            cout << tp.exchange->name() << ":" << tp.sell << ">" << tp.buy << "@" << tp.ask << ", ";
         }
         cout << "Net Change:" << net_gain << endl;
     }
@@ -65,16 +62,17 @@ class crypto_market {
     vector<trade_pair> pairs;
     vector<trade_seq> seqs;
 
-    void add_pair(trade_pair &new_trade_pair){
+
+    void add_pair(trade_pair new_trade_pair){
         pairs.push_back(new_trade_pair);
     }
-    void add_seq(trade_seq &new_trade_seq){
+    void add_seq(trade_seq new_trade_seq){
         seqs.push_back(new_trade_seq);
     }
 
     void list_pairs() {
         for(const trade_pair& tp : pairs){
-            cout << tp.exchange << " " << tp.buy << "-" << tp.sell << " " << tp.bid << endl;
+            cout << (*tp.exchange).name() << " " << tp.buy << "-" << tp.sell << " " << tp.bid << endl;
         }
     }
 };
@@ -82,10 +80,10 @@ class crypto_market {
 void build_graph(crypto_market &market){
     cout << "Building Graph..." << endl;
 
-    crypto_exchange coinbase("coinbase");
+    crypto_exchange* coinbase = new crypto_exchange("coinbase");
 
     trade_pair tp1;
-    tp1.exchange = &coinbase;
+    tp1.exchange = coinbase;
     tp1.buy = "BTC";
     tp1.sell = "USD";
     tp1.bid = 0.000099;
@@ -93,7 +91,7 @@ void build_graph(crypto_market &market){
     market.add_pair(tp1);
 
     trade_pair tp1r;
-    tp1r.exchange = &coinbase;
+    tp1r.exchange = coinbase;
     tp1r.buy = "USD";
     tp1r.sell = "BTC";
     tp1r.bid = 10100.49;
@@ -101,7 +99,7 @@ void build_graph(crypto_market &market){
     market.add_pair(tp1r);
 
     trade_pair tp2;
-    tp2.exchange = &coinbase;
+    tp2.exchange = coinbase;
     tp2.buy = "BTC";
     tp2.sell = "BCH";
     tp2.bid = 0.149;
@@ -109,7 +107,7 @@ void build_graph(crypto_market &market){
     market.add_pair(tp2);
 
     trade_pair tp2r;
-    tp2r.exchange = &coinbase;
+    tp2r.exchange = coinbase;
     tp2r.buy = "BCH";
     tp2r.sell = "BTC";
     tp2r.bid = 6.66;
@@ -117,7 +115,7 @@ void build_graph(crypto_market &market){
     market.add_pair(tp2r);
 
     trade_pair tp3;
-    tp3.exchange = &coinbase;
+    tp3.exchange = coinbase;
     tp3.buy = "USD";
     tp3.sell = "BCH";
     tp3.bid = 1600;
@@ -125,7 +123,7 @@ void build_graph(crypto_market &market){
     market.add_pair(tp3);
 
     trade_pair tp3r;
-    tp3r.exchange = &coinbase;
+    tp3r.exchange = coinbase;
     tp3r.buy = "BCH";
     tp3r.sell = "USD";
     tp3r.bid = 0.000625;
@@ -138,12 +136,12 @@ void traverse_graph(crypto_market& market){
     cout << "Traversing Graph..." << endl;
 
     //Naieve O(n^3) terrible graph traversal
-    for(trade_pair& tp1 : market.pairs){
-        for(trade_pair& tp2 : market.pairs) {
+    for(const trade_pair& tp1 : market.pairs){
+        for(const trade_pair& tp2 : market.pairs) {
             if(tp1.buy != tp2.sell){
                 continue;
             }
-            for(trade_pair& tp3 : market.pairs){
+            for(const trade_pair& tp3 : market.pairs){
                 if(tp2.buy != tp3.sell){
                     continue;
                 }
@@ -151,13 +149,13 @@ void traverse_graph(crypto_market& market){
                 if(tp3.buy != tp1.sell) {
                     continue;
                 }
-                cout << "Saving Seq..." << endl;
                 trade_seq ts;
-                ts.add(tp1);
-                ts.add(tp2);
-                ts.add(tp3);
+                ts.add_pair(tp1);
+                ts.add_pair(tp2);
+                ts.add_pair(tp3);
                 ts.print_seq();
                 market.add_seq(ts);
+
             }
         }
     }
@@ -176,13 +174,12 @@ void execute_trades(crypto_market& market) {
     for(it = market.seqs.begin(); it != market.seqs.end(); ++it){
         if(count == 0){
             most_profitable = &(*it);
-        }else if(it->net_gain > most_profitable->net_gain){
+        }else if((*it).net_gain > most_profitable->net_gain){
             most_profitable = &(*it);
         }
         ++count;
     }
     
-    cout << "Excuting Trade:" << endl;
     most_profitable->print_seq();
 }
 
@@ -192,6 +189,7 @@ int main(int argc, char* argv[]){
     crypto_market market; 
 
     build_graph(market);
+    market.list_pairs();
 
     traverse_graph(market);
 
