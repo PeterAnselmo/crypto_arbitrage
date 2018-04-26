9e0a587a6e98790052befbab4db7679329b56dea
Peter Anselmo <peter@anselmo.me>
2018-04-26 14:05:25 -0700

    Adding buy handling to test web data, moving more to be dynamic

--- test/web_data/poloniex/tradingapi.php
+++ test/web_data/poloniex/tradingapi.php
@@ -4,27 +4,51 @@ $balances = array(
     "XRP"=>"50.0",
     "BCH"=>"2.5",
     "LTC"=>"3.25",
-    "ETH"=>"0.0000000")
-?>
-<?php if($_POST['command'] == 'returnBalances'){ ?>
-<?php echo json_encode($balances) ?>
-<?php } else if($_POST['command'] == 'returnFeeInfo'){ ?>
-{"makerFee": "0.00140000", "takerFee": "0.00250000", "thirtyDayVolume": "612.00248891", "nextTier": "1200.00000000"}
-<?php 
+    "ETH"=>"0.0000000");
+$market_rates = array(
+    "BTC_XMR" => 0.02988499,
+    "BTC_XRP" => 0.00008996,
+    "BTC_ZEC" => 0.0330);
+
+if($_POST['command'] == 'returnBalances'){
+    echo json_encode($balances);
+
+} else if($_POST['command'] == 'returnFeeInfo'){
+    echo '{"makerFee": "0.00140000", "takerFee": "0.00250000", "thirtyDayVolume": "612.00248891", "nextTier": "1200.00000000"}';
 
 //command=sell&currencyPair=BTC_XRP&rate=0.00009277&amount=179.69999695&immediateOrCancel=1&nonce=1524721917678
 } else if ($_POST['command'] == 'sell'){
-    if($_POST['rate'] > 0.00009979){
+    if($_POST['rate'] <= $market_rates[$_POST['currencyPair']]){
+        //command=sell&currencyPair=BTC_XRP&rate=0.00008990&amount=179.69999695&immediateOrCancel=1&nonce=1524724856176
+        echo '{"orderNumber":"144492489990","resultingTrades":[{"amount":"179.69999695","date":"2018-04-26 06:40:57","rate":"0.00008996","total":"0.01616581","tradeID":"21662264","type":"sell"}],"amountUnfilled":"0.00000000"}';
+    } else {
         $response = array(
             "orderNumber"=>"144484516971",
             "resultingTrades"=>[],
             "amountUnfilled"=>$_POST["amount"]);
         echo json_encode($response);
-    } else {
-        //command=sell&currencyPair=BTC_XRP&rate=0.00008990&amount=179.69999695&immediateOrCancel=1&nonce=1524724856176
-        echo '{"orderNumber":"144492489990","resultingTrades":[{"amount":"179.69999695","date":"2018-04-26 06:40:57","rate":"0.00008996","total":"0.01616581","tradeID":"21662264","type":"sell"}],"amountUnfilled":"0.00000000"}';
-
     }
+
+//command=buy&currencyPair=BTC_ZEC&rate=0.03009176&amount=0.34958404&immediateOrCancel=1&nonce=1524771128271
+} else if ($_POST['command'] == 'buy'){
+    if($_POST['rate'] >= $market_rates[$_POST['currencyPair']]){
+        $response = array(
+            "orderNumber" => "270275494074",
+            "resultingTrades"=>array(array(
+                "amount"=> $_POST['amount'],
+                "date"=>"2018-04-26 19:52:58",
+                "rate"=> strval($market_rates[$_POST['currencyPair']]),
+                "total"=> strval($market_rates[$_POST['currencyPair']] * $_POST['amount']),
+                "tradeID"=>"16843227",
+                "type"=>"buy")),
+            "amountUnfilled"=>"0.00000000");
+    } else {
+        $response = array(
+            "orderNumber"=>"119971507579",
+            "resultingTrades"=>[],
+            "amountUnfilled"=>$_POST["amount"]);
+    }
+    echo json_encode($response);
 } else {
     echo var_dump($_POST);
 }
